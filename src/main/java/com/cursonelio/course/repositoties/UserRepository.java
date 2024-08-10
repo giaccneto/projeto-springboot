860cab9485d1fc56cd0b5bd2b1647c35787a5d94
Giacomo <giaccneto@hotmail.com>
2024-08-10 10:02:13 -0400

    Service layer, component registration

--- src/main/java/com/cursonelio/course/repositoties/UserRepository.java
+++ src/main/java/com/cursonelio/course/repositoties/UserRepository.java
@@ -3,6 +3,7 @@ package com.cursonelio.course.repositoties;
 import com.cursonelio.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
